f019d46ac830a152c63b0a116dcef39c2c4e8a39
hxxcxx <hxxcxx@126.com>
2026-03-06 00:39:45 +0800

    Refactor sweep line status to use SegmentInfo struct

--- src/intersection/line_segment_intersection.cc
+++ src/intersection/line_segment_intersection.cc
@@ -14,28 +14,42 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
     return intersections;
   }
   
-  // Current sweep line x position (needed for ordering)
-  double sweep_x = 0.0;
+  // Segment info for sweep line ordering
+  struct SegmentInfo {
+    int index;
+    const std::vector<Edge2D>* segments_ptr;
+    double sweep_x;  // Current sweep line position
+    
+    SegmentInfo(int idx, const std::vector<Edge2D>* segs, double x)
+        : index(idx), segments_ptr(segs), sweep_x(x) {}
+    
+    // Get y-coordinate at current sweep_x
+    double getY() const {
+      return GetYAtX((*segments_ptr)[index], sweep_x);
+    }
+    
+    // Get slope
+    double getSlope() const {
+      const Edge2D& seg = (*segments_ptr)[index];
+      return (seg.p2.y - seg.p1.y) / (seg.p2.x - seg.p1.x);
+    }
+  };
   
-  // Comparator for sweep line (orders segments by y at current sweep_x)
-  auto segment_comparator = [&sweep_x, &segments](int a, int b) {
-    double y_a = GetYAtX(segments[a], sweep_x);
-    double y_b = GetYAtX(segments[b], sweep_x);
+  // Comparator for sweep line (BBST)
+  auto segment_comparator = [](const SegmentInfo& a, const SegmentInfo& b) {
+    double y_a = a.getY();
+    double y_b = b.getY();
     if (std::abs(y_a - y_b) > 1e-10) {
       return y_a < y_b;
     }
     // If y coordinates are equal, compare by slope
-    const Edge2D& seg1 = segments[a];
-    const Edge2D& seg2 = segments[b];
-    double slope1 = (seg1.p2.y - seg1.p1.y) / (seg1.p2.x - seg1.p1.x);
-    double slope2 = (seg2.p2.y - seg2.p1.y) / (seg2.p2.x - seg2.p1.x);
-    return slope1 < slope2;
+    return a.getSlope() < b.getSlope();
   };
   
-  // Sweep line: set of segment indices ordered by y at current x
-  std::set<int, decltype(segment_comparator)> sweep_line(segment_comparator);
+  // Sweep line: BBST (std::set is a red-black tree)
+  std::set<SegmentInfo, decltype(segment_comparator)> status(segment_comparator);
   
-  // Initialize event queue with all segment endpoints
+  // Initialize event queue with all endpoints
   std::priority_queue<EventPoint, std::vector<EventPoint>, std::greater<EventPoint>> event_queue;
   
   for (size_t i = 0; i < segments.size(); ++i) {
@@ -59,7 +73,7 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
     }
   }
   
-  // Track found intersections to avoid duplicates
+  // Track found intersections
   std::set<std::pair<int, int>> found_intersections;
   
   // Process events
@@ -67,76 +81,88 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
     EventPoint event = event_queue.top();
     event_queue.pop();
     
-    sweep_x = event.point.x;
+    double current_x = event.point.x;
+    
+    // Update sweep_x for all segments in status
+    // Since SegmentInfo stores sweep_x, we need to rebuild
+    std::vector<int> segments_in_status;
+    segments_in_status.reserve(status.size());
+    for (const auto& info : status) {
+      segments_in_status.push_back(info.index);
+    }
+    status.clear();
+    for (int seg_idx : segments_in_status) {
+      status.emplace(seg_idx, &segments, current_x);
+    }
     
     if (event.type == EventType::LeftEndpoint) {
-      // Insert segment into sweep line
-      int seg_idx = event.segment_index;
-      auto it = sweep_line.insert(seg_idx).first;
+      // Insert segment into status
+      auto it = status.emplace(event.segment_index, &segments, current_x).first;
       
-      // Check predecessor
-      if (it != sweep_line.begin()) {
+      // Get predecessor (using BBST iterator)
+      if (it != status.begin()) {
         auto prev = it;
         --prev;
         Point2D intersection;
-        if (SegmentsIntersect(segments[seg_idx], segments[*prev], intersection)) {
-          auto pair = std::minmax(seg_idx, *prev);
+        if (SegmentsIntersect(segments[event.segment_index], segments[prev->index], intersection)) {
+          auto pair = std::minmax(event.segment_index, prev->index);
           if (found_intersections.find(pair) == found_intersections.end()) {
             found_intersections.insert(pair);
-            intersections.emplace_back(intersection, seg_idx, *prev);
-            // Add intersection event if it's to the right
-            if (intersection.x > sweep_x + 1e-10) {
-              event_queue.emplace(intersection, seg_idx, *prev);
+            intersections.emplace_back(intersection, event.segment_index, prev->index);
+            if (intersection.x > current_x + 1e-10) {
+              event_queue.emplace(intersection, event.segment_index, prev->index);
             }
           }
         }
       }
       
-      // Check successor
+      // Get successor (using BBST iterator)
       auto next = it;
       ++next;
-      if (next != sweep_line.end()) {
+      if (next != status.end()) {
         Point2D intersection;
-        if (SegmentsIntersect(segments[seg_idx], segments[*next], intersection)) {
-          auto pair = std::minmax(seg_idx, *next);
+        if (SegmentsIntersect(segments[event.segment_index], segments[next->index], intersection)) {
+          auto pair = std::minmax(event.segment_index, next->index);
           if (found_intersections.find(pair) == found_intersections.end()) {
             found_intersections.insert(pair);
-            intersections.emplace_back(intersection, seg_idx, *next);
-            if (intersection.x > sweep_x + 1e-10) {
-              event_queue.emplace(intersection, seg_idx, *next);
+            intersections.emplace_back(intersection, event.segment_index, next->index);
+            if (intersection.x > current_x + 1e-10) {
+              event_queue.emplace(intersection, event.segment_index, next->index);
             }
           }
         }
       }
       
     } else if (event.type == EventType::RightEndpoint) {
-      // Remove segment from sweep line
-      int seg_idx = event.segment_index;
-      auto it = sweep_line.find(seg_idx);
+      // Find segment to remove
+      SegmentInfo target(event.segment_index, &segments, current_x);
+      auto it = status.find(target);
       
-      if (it != sweep_line.end()) {
+      if (it != status.end()) {
+        // Get predecessor and successor before removal (BBST operations)
         auto prev = it;
         auto next = it;
         ++next;
         
-        if (it != sweep_line.begin()) {
+        if (it != status.begin()) {
           --prev;
         } else {
-          prev = sweep_line.end();
+          prev = status.end();
         }
         
-        sweep_line.erase(it);
+        // Remove segment from BBST
+        status.erase(it);
         
-        // Check if neighbors now intersect
-        if (prev != sweep_line.end() && next != sweep_line.end()) {
+        // Check if predecessor and successor now intersect
+        if (prev != status.end() && next != status.end()) {
           Point2D intersection;
-          if (SegmentsIntersect(segments[*prev], segments[*next], intersection)) {
-            auto pair = std::minmax(*prev, *next);
+          if (SegmentsIntersect(segments[prev->index], segments[next->index], intersection)) {
+            auto pair = std::minmax(prev->index, next->index);
             if (found_intersections.find(pair) == found_intersections.end()) {
               found_intersections.insert(pair);
-              intersections.emplace_back(intersection, *prev, *next);
-              if (intersection.x > sweep_x + 1e-10) {
-                event_queue.emplace(intersection, *prev, *next);
+              intersections.emplace_back(intersection, prev->index, next->index);
+              if (intersection.x > current_x + 1e-10) {
+                event_queue.emplace(intersection, prev->index, next->index);
               }
             }
           }
@@ -144,44 +170,52 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
       }
       
     } else {  // EventType::Intersection
-      // Two segments intersect - they will swap order in sweep line
-      int seg1_idx = event.segment_index;
-      int seg2_idx = event.segment_index2;
+      // Two segments intersect - swap their order in status
+      int seg1 = event.segment_index;
+      int seg2 = event.segment_index2;
       
-      // Find and remove both segments
-      auto it1 = sweep_line.find(seg1_idx);
-      auto it2 = sweep_line.find(seg2_idx);
+      // Find both segments
+      SegmentInfo info1(seg1, &segments, current_x);
+      SegmentInfo info2(seg2, &segments, current_x);
       
-      if (it1 != sweep_line.end() && it2 != sweep_line.end()) {
-        // Get neighbors before removing
+      auto it1 = status.find(info1);
+      auto it2 = status.find(info2);
+      
+      if (it1 != status.end() && it2 != status.end()) {
+        // Get neighbors before removal (BBST operations)
         auto prev1 = it1, next1 = it1;
         auto prev2 = it2, next2 = it2;
         
-        if (it1 != sweep_line.begin()) { --prev1; } else { prev1 = sweep_line.end(); }
-        if (it2 != sweep_line.begin()) { --prev2; } else { prev2 = sweep_line.end(); }
+        if (it1 != status.begin()) { --prev1; } else { prev1 = status.end(); }
+        if (it2 != status.begin()) { --prev2; } else { prev2 = status.end(); }
         ++next1;
         ++next2;
         
-        // Remove and re-insert (order will swap due to intersection)
-        sweep_line.erase(it1);
-        sweep_line.erase(it2);
-        sweep_line.insert(seg1_idx);
-        sweep_line.insert(seg2_idx);
+        // Remove both segments from BBST
+        status.erase(it1);
+        status.erase(it2);
+        
+        // Re-insert (order will swap after intersection point)
+        status.emplace(seg1, &segments, current_x);
+        status.emplace(seg2, &segments, current_x);
+        
+        // Find new positions and check new neighbors
+        auto new_it1 = status.find(SegmentInfo(seg1, &segments, current_x));
+        auto new_it2 = status.find(SegmentInfo(seg2, &segments, current_x));
         
         // Check new neighbors of seg1
-        auto new_it1 = sweep_line.find(seg1_idx);
-        if (new_it1 != sweep_line.begin()) {
+        if (new_it1 != status.begin()) {
           auto prev = new_it1;
           --prev;
-          if (*prev != seg2_idx) {
+          if (prev->index != seg2) {
             Point2D intersection;
-            if (SegmentsIntersect(segments[seg1_idx], segments[*prev], intersection)) {
-              auto pair = std::minmax(seg1_idx, *prev);
+            if (SegmentsIntersect(segments[seg1], segments[prev->index], intersection)) {
+              auto pair = std::minmax(seg1, prev->index);
               if (found_intersections.find(pair) == found_intersections.end()) {
                 found_intersections.insert(pair);
-                intersections.emplace_back(intersection, seg1_idx, *prev);
-                if (intersection.x > sweep_x + 1e-10) {
-                  event_queue.emplace(intersection, seg1_idx, *prev);
+                intersections.emplace_back(intersection, seg1, prev->index);
+                if (intersection.x > current_x + 1e-10) {
+                  event_queue.emplace(intersection, seg1, prev->index);
                 }
               }
             }
@@ -190,34 +224,33 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
         
         auto next = new_it1;
         ++next;
-        if (next != sweep_line.end() && *next != seg2_idx) {
+        if (next != status.end() && next->index != seg2) {
           Point2D intersection;
-          if (SegmentsIntersect(segments[seg1_idx], segments[*next], intersection)) {
-            auto pair = std::minmax(seg1_idx, *next);
+          if (SegmentsIntersect(segments[seg1], segments[next->index], intersection)) {
+            auto pair = std::minmax(seg1, next->index);
             if (found_intersections.find(pair) == found_intersections.end()) {
               found_intersections.insert(pair);
-              intersections.emplace_back(intersection, seg1_idx, *next);
-              if (intersection.x > sweep_x + 1e-10) {
-                event_queue.emplace(intersection, seg1_idx, *next);
+              intersections.emplace_back(intersection, seg1, next->index);
+              if (intersection.x > current_x + 1e-10) {
+                event_queue.emplace(intersection, seg1, next->index);
               }
             }
           }
         }
         
         // Check new neighbors of seg2
-        auto new_it2 = sweep_line.find(seg2_idx);
-        if (new_it2 != sweep_line.begin()) {
+        if (new_it2 != status.begin()) {
           auto prev = new_it2;
           --prev;
-          if (*prev != seg1_idx) {
+          if (prev->index != seg1) {
             Point2D intersection;
-            if (SegmentsIntersect(segments[seg2_idx], segments[*prev], intersection)) {
-              auto pair = std::minmax(seg2_idx, *prev);
+            if (SegmentsIntersect(segments[seg2], segments[prev->index], intersection)) {
+              auto pair = std::minmax(seg2, prev->index);
               if (found_intersections.find(pair) == found_intersections.end()) {
                 found_intersections.insert(pair);
-                intersections.emplace_back(intersection, seg2_idx, *prev);
-                if (intersection.x > sweep_x + 1e-10) {
-                  event_queue.emplace(intersection, seg2_idx, *prev);
+                intersections.emplace_back(intersection, seg2, prev->index);
+                if (intersection.x > current_x + 1e-10) {
+                  event_queue.emplace(intersection, seg2, prev->index);
                 }
               }
             }
@@ -226,15 +259,15 @@ std::vector<IntersectionPoint> LineSegmentIntersection::FindAllIntersections(
         
         next = new_it2;
         ++next;
-        if (next != sweep_line.end() && *next != seg1_idx) {
+        if (next != status.end() && next->index != seg1) {
           Point2D intersection;
-          if (SegmentsIntersect(segments[seg2_idx], segments[*next], intersection)) {
-            auto pair = std::minmax(seg2_idx, *next);
+          if (SegmentsIntersect(segments[seg2], segments[next->index], intersection)) {
+            auto pair = std::minmax(seg2, next->index);
             if (found_intersections.find(pair) == found_intersections.end()) {
               found_intersections.insert(pair);
-              intersections.emplace_back(intersection, seg2_idx, *next);
-              if (intersection.x > sweep_x + 1e-10) {
-                event_queue.emplace(intersection, seg2_idx, *next);
+              intersections.emplace_back(intersection, seg2, next->index);
+              if (intersection.x > current_x + 1e-10) {
+                event_queue.emplace(intersection, seg2, next->index);
               }
             }
           }
